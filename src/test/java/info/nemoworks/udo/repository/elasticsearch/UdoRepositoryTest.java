f43ec045d9bcfc9269943536d543b28763b8bb86
NovelistChan <774527381@qq.com>
2021-07-18 19:16:39 +0800

    connect 2.0 modify

--- src/test/java/info/nemoworks/udo/repository/elasticsearch/UdoRepositoryTest.java
+++ src/test/java/info/nemoworks/udo/repository/elasticsearch/UdoRepositoryTest.java
@@ -1,31 +1,24 @@
 package info.nemoworks.udo.repository.elasticsearch;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-
-
+import info.nemoworks.udo.model.Udo;
+import info.nemoworks.udo.model.UdoType;
 import info.nemoworks.udo.storage.UdoNotExistException;
+import info.nemoworks.udo.storage.UdoPersistException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.elasticsearch.ElasticsearchContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import info.nemoworks.udo.model.Udo;
-import info.nemoworks.udo.model.UdoType;
-import info.nemoworks.udo.storage.UdoPersistException;
-
-import java.util.List;
-
 @Import(ElasticsearchConfig.class)
-@SpringBootTest(classes = { info.nemoworks.udo.repository.elasticsearch.UdoWrapperRepository.class })
+@SpringBootTest(classes = {info.nemoworks.udo.repository.elasticsearch.UdoWrapperRepository.class})
 @Testcontainers
 public class UdoRepositoryTest {
 
@@ -57,17 +50,17 @@ public class UdoRepositoryTest {
     @Test
     public void insetOneSchema() throws UdoPersistException, UdoNotExistException {
         String jsonString = "{'id': 1001, "
-                + "'firstName': 'Lokesh',"
-                + "'lastName': 'Gupta',"
-                + "'email': 'howtodoinjava@gmail.com'}";
-        JsonObject data = new Gson().fromJson(jsonString,JsonObject.class);
+            + "'firstName': 'Lokesh',"
+            + "'lastName': 'Gupta',"
+            + "'email': 'howtodoinjava@gmail.com'}";
+        JsonObject data = new Gson().fromJson(jsonString, JsonObject.class);
         UdoType type = new UdoType(data);
         UdoType udoType = repository.saveType(type);
         System.out.println(udoType.toJsonObject());
     }
 
     @Test
-    public void testGetUdosByType(){
+    public void testGetUdosByType() {
         String udoTypeId = "d3CkaXkBwWaSNh-CvheE";
         UdoType udoType = repository.findTypeById(udoTypeId);
         List<Udo> udos = repository.findUdosByType(udoType);
@@ -80,24 +73,24 @@ public class UdoRepositoryTest {
     public void testUpdateUdoType() throws UdoPersistException {
         String udoTypeId = "d3CkaXkBwWaSNh-CvheE";
         String s = "{\n" +
-                "    \"type\": \"object\",\n" +
-                "    \"title\": \"air_purifier\",\n" +
-                "    \"properties\": {\n" +
-                "        \"Name\": {\n" +
-                "            \"type\": \"string\",\n" +
-                "            \"description\": \"产品名称\"\n" +
-                "        },\n" +
-                "        \"Brand\": {\n" +
-                "            \"type\": \"string\",\n" +
-                "            \"description\": \"品牌\"\n" +
-                "        },\n" +
-                "        \"Version\": {\n" +
-                "            \"type\": \"string\",\n" +
-                "            \"description\": \"型号\"\n" +
-                "        }\n" +
-                "    }\n" +
-                "}";
-        JsonObject data = new Gson().fromJson(s,JsonObject.class);
+            "    \"type\": \"object\",\n" +
+            "    \"title\": \"air_purifier\",\n" +
+            "    \"properties\": {\n" +
+            "        \"Name\": {\n" +
+            "            \"type\": \"string\",\n" +
+            "            \"description\": \"产品名称\"\n" +
+            "        },\n" +
+            "        \"Brand\": {\n" +
+            "            \"type\": \"string\",\n" +
+            "            \"description\": \"品牌\"\n" +
+            "        },\n" +
+            "        \"Version\": {\n" +
+            "            \"type\": \"string\",\n" +
+            "            \"description\": \"型号\"\n" +
+            "        }\n" +
+            "    }\n" +
+            "}";
+        JsonObject data = new Gson().fromJson(s, JsonObject.class);
         UdoType type = new UdoType(data);
         UdoType udoType = repository.saveType(type);
         System.out.println(udoType);
@@ -106,10 +99,10 @@ public class UdoRepositoryTest {
     @Test
     public void testGetTypeById() throws UdoPersistException {
         String jsonString = "{'id': 1001, "
-                + "'firstName': 'Lokest',"
-                + "'lastName': 'Guptt',"
-                + "'email': 'howtodoonjava@gmail.com'}";
-        JsonObject data = new Gson().fromJson(jsonString,JsonObject.class);
+            + "'firstName': 'Lokest',"
+            + "'lastName': 'Guptt',"
+            + "'email': 'howtodoonjava@gmail.com'}";
+        JsonObject data = new Gson().fromJson(jsonString, JsonObject.class);
         UdoType type = new UdoType(data);
         UdoType udoType = repository.saveType(type);
         UdoType typeById = repository.findTypeById(udoType.getId());
@@ -119,10 +112,10 @@ public class UdoRepositoryTest {
     @Test
     public void testFindAllTypes() throws UdoPersistException {
         String jsonString = "{'id': 1001, "
-                + "'firstName': 'Lokesh',"
-                + "'lastName': 'Gupta',"
-                + "'email': 'howtodoinjava@gmail.com'}";
-        JsonObject data = new Gson().fromJson(jsonString,JsonObject.class);
+            + "'firstName': 'Lokesh',"
+            + "'lastName': 'Gupta',"
+            + "'email': 'howtodoinjava@gmail.com'}";
+        JsonObject data = new Gson().fromJson(jsonString, JsonObject.class);
         UdoType type = new UdoType(data);
         UdoType udoSchema = repository.saveType(type);
         repository.findAllTypes().forEach(udotype1 -> {
@@ -133,14 +126,14 @@ public class UdoRepositoryTest {
     @Test
     public void testDeleteTypeById() throws UdoPersistException, UdoNotExistException {
         String jsonString = "{'id': 1001, "
-                + "'firstName': 'Lokesh',"
-                + "'lastName': 'Gupta',"
-                + "'email': 'howtodoinjava@gmail.com'}";
-        JsonObject data = new Gson().fromJson(jsonString,JsonObject.class);
+            + "'firstName': 'Lokesh',"
+            + "'lastName': 'Gupta',"
+            + "'email': 'howtodoinjava@gmail.com'}";
+        JsonObject data = new Gson().fromJson(jsonString, JsonObject.class);
         UdoType schema = new UdoType(data);
         UdoType udoSchema = repository.saveType(schema);
         repository.deleteTypeById(udoSchema.getId());
-        logger.info("number of schemas: "+String.valueOf(repository.findAllTypes().size()));
+        logger.info("number of schemas: " + String.valueOf(repository.findAllTypes().size()));
     }
 
 
@@ -148,66 +141,66 @@ public class UdoRepositoryTest {
     public void insertOneUdo() throws UdoPersistException, UdoNotExistException {
 
         String jsonString = "{\n" +
-                "  \"type\": \"object\",\n" +
-                "  \"title\": \"VirtualMachineInstance\",\n" +
-                "  \"properties\": {\n" +
-                "    \"CPU\": {\n" +
-                "      \"properties\": {\n" +
-                "        \"cores\": {\n" +
-                "          \"type\": \"integer\"\n" +
-                "        },\n" +
-                "        \"sockets\": {\n" +
-                "          \"type\": \"integer\"\n" +
-                "        },\n" +
-                "        \"threads\": {\n" +
-                "          \"type\": \"integer\"\n" +
-                "        },\n" +
-                "        \"model\": {\n" +
-                "          \"type\": \"string\"\n" +
-                "        },\n" +
-                "        \"features\": {\n" +
-                "          \"items\": {\n" +
-                "            \"type\": \"embedded\",\n" +
-                "            \"typeName\": \"CPUFeature\"\n" +
-                "          },\n" +
-                "          \"type\": \"array\"\n" +
-                "        },\n" +
-                "        \"dedicatedCpuPlacement\": {\n" +
-                "          \"type\": \"boolean\"\n" +
-                "        },\n" +
-                "        \"isolateEmulatorThread\": {\n" +
-                "          \"type\": \"boolean\"\n" +
-                "        }\n" +
-                "      },\n" +
-                "      \"additionalProperties\": false,\n" +
-                "      \"title\": \"CPU\",\n" +
-                "      \"type\": \"object\"\n" +
-                "    },\n" +
-                "    \"CPUFeature\": {\n" +
-                "      \"required\": [\n" +
-                "        \"name\"\n" +
-                "      ],\n" +
-                "      \"properties\": {\n" +
-                "        \"name\": {\n" +
-                "          \"type\": \"string\"\n" +
-                "        },\n" +
-                "        \"policy\": {\n" +
-                "          \"type\": \"string\"\n" +
-                "        }\n" +
-                "      },\n" +
-                "      \"additionalProperties\": false,\n" +
-                "      \"title\": \"CPUFeature\",\n" +
-                "      \"type\": \"object\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
+            "  \"type\": \"object\",\n" +
+            "  \"title\": \"VirtualMachineInstance\",\n" +
+            "  \"properties\": {\n" +
+            "    \"CPU\": {\n" +
+            "      \"properties\": {\n" +
+            "        \"cores\": {\n" +
+            "          \"type\": \"integer\"\n" +
+            "        },\n" +
+            "        \"sockets\": {\n" +
+            "          \"type\": \"integer\"\n" +
+            "        },\n" +
+            "        \"threads\": {\n" +
+            "          \"type\": \"integer\"\n" +
+            "        },\n" +
+            "        \"model\": {\n" +
+            "          \"type\": \"string\"\n" +
+            "        },\n" +
+            "        \"features\": {\n" +
+            "          \"items\": {\n" +
+            "            \"type\": \"embedded\",\n" +
+            "            \"typeName\": \"CPUFeature\"\n" +
+            "          },\n" +
+            "          \"type\": \"array\"\n" +
+            "        },\n" +
+            "        \"dedicatedCpuPlacement\": {\n" +
+            "          \"type\": \"boolean\"\n" +
+            "        },\n" +
+            "        \"isolateEmulatorThread\": {\n" +
+            "          \"type\": \"boolean\"\n" +
+            "        }\n" +
+            "      },\n" +
+            "      \"additionalProperties\": false,\n" +
+            "      \"title\": \"CPU\",\n" +
+            "      \"type\": \"object\"\n" +
+            "    },\n" +
+            "    \"CPUFeature\": {\n" +
+            "      \"required\": [\n" +
+            "        \"name\"\n" +
+            "      ],\n" +
+            "      \"properties\": {\n" +
+            "        \"name\": {\n" +
+            "          \"type\": \"string\"\n" +
+            "        },\n" +
+            "        \"policy\": {\n" +
+            "          \"type\": \"string\"\n" +
+            "        }\n" +
+            "      },\n" +
+            "      \"additionalProperties\": false,\n" +
+            "      \"title\": \"CPUFeature\",\n" +
+            "      \"type\": \"object\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
 
-        JsonObject data = new Gson().fromJson(jsonString,JsonObject.class);
+        JsonObject data = new Gson().fromJson(jsonString, JsonObject.class);
         UdoType type = new UdoType(data);
         type.setId("schema-1");
 
         Udo udo = new Udo(type, data);
-        udo.uri = "http://localhost:8081/";
+//        udo.uri = "http://localhost:8081/";
         assertNotNull(udo);
         Udo udo1 = repository.saveUdo(udo);
         System.out.println(udo1.toJsonObject());
